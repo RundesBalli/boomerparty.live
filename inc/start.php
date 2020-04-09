2fdf230da41236399da57492750dd0af4885b149
RundesBalli <rundesballi@rundesballi.com>
2020-04-09 23:21:51 +0200

    Dateien umbenannt, da manche Adblocker wohl die Bilder blockieren.

--- inc/start.php
+++ inc/start.php
@@ -5,12 +5,17 @@
  * Startseite
  */
 
+/**
+ * Spotify Link
+ */
 $content.= "<div class='imglink'>".PHP_EOL.
-"<a href='https://open.spotify.com/playlist/64WfneVI8dqmpd6T6QKAMs' target='_blank' rel='noopener'><img src='/src/spotify.png' alt='Spotify'></a>".PHP_EOL.
+"<a href='https://open.spotify.com/playlist/64WfneVI8dqmpd6T6QKAMs' target='_blank' rel='noopener'><img src='/src/button_SpotifyPlaylist.png' alt='Spotify'></a>".PHP_EOL.
 "</div>";
 
+/**
+ * YouTube Link
+ */
 $content.= "<div class='imglink'>".PHP_EOL.
-"<a href='https://www.youtube.com/playlist?list=PLOnRDACaCcSMom4d1cAxxaoFjeP9167I8' target='_blank' rel='noopener'><img src='/src/youtube.png' alt='YouTube'></a>".PHP_EOL.
+"<a href='https://www.youtube.com/playlist?list=PLOnRDACaCcSMom4d1cAxxaoFjeP9167I8' target='_blank' rel='noopener'><img src='/src/button_YouTubePlaylist.png' alt='YouTube'></a>".PHP_EOL.
 "</div>";
-
 ?>
